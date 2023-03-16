63507714baae30bb6fcbc89e54f1956b1caa6f6f
onzerebrian <onzerebrian@gmail.com>
2023-03-16 10:06:23 -0700

    last

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
  * main - Entry point
  * Return: Always 1 (Success)
  */
-int main(void)
+int main()
 {
 	write(STDOUT_FILENO, "and that piece of art is useful
 			\" - Dora Korpar, 2015-10-19\n", 59);
