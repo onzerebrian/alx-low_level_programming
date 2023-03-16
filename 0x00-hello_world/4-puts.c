9745feaca03a0316f004a9cae2b649bba2d11a12
onzerebrian <onzerebrian@gmail.com>
2023-03-16 09:38:50 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 /*
  * header file
- */
+ * the main is is of int datatype
+*/
 int main(void)
 {
+	/*
+ * header file
+ * the main is is of int datatype
+*/
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
