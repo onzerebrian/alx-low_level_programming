a68e64b1e1eb7ad5b6dbb5f7bf2be3fac96ee861
onzerebrian <onzerebrian@gmail.com>
2023-03-17 01:06:19 -0700

    last

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,10 @@
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
  * main - Entry point
  * Return: Always 1 (Success)
  */
-int main()
+int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\r\n", 58)
+return (1);
 }
