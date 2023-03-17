8883441257f53740c256f23960fa2db8f3001f48
onzerebrian <onzerebrian@gmail.com>
2023-03-17 14:15:18 -0700

    alphabets in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - This is where the program begins
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char m;
+
+
+	for (m = 'z'; m >= 'a'; m--)
+	{
+		putchar (m);
+	}
+	putchar('\n');
+	return (0);
+}
