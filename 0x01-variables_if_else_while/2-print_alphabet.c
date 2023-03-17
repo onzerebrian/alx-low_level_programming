5fd2cab15b6f7558ea858c294b0e29d2b2e5f715
onzerebrian <onzerebrian@gmail.com>
2023-03-17 13:10:49 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - This is where the program begins
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		puchar (n)
+	}
+	return (0);
+}
