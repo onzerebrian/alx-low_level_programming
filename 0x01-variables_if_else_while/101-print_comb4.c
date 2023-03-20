44c383f2671cf88e1679164e3e733428fc8508d4
onzerebrian <onzerebrian@gmail.com>
2023-03-19 22:41:35 -0700

    fixed three digit number output

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - This is where the program begins
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int m;
+	int n;
+	int k;
+
+	for (m = 0; m < 10; m++)
+	{
+		n = m + 1;
+		for (n = n; n < 10; n++)
+		{
+			k = n + 1;
+			for (k = k; k < 9; k++)
+			{
+			putchar ('0' + m);
+			putchar ('0' + n);
+			putchar ('0' + k);
+
+			if (m < 8)
+			{
+				putchar (',');
+				putchar (' ');
+			}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
