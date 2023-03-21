1a028d6a4a4188f7d99c2e78e33a3a0e0fb53764
onzerebrian <onzerebrian@gmail.com>
2023-03-21 13:52:07 -0700

    mn

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _islower - Checks for lowercase character
+ * _isalpha - Checks for lowercase character
  *
  * @c: The character to be checked
  *
  *Return: 1 for lowercase character or 0 for anything else
  */
 
-int _islower(int c)
+int _isalpha(int c)
 {
 	int no;
 
