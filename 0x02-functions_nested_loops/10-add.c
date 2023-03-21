024a22b098b15bb839c256a0cc959d2e6e67e408
onzerebrian <onzerebrian@gmail.com>
2023-03-21 14:43:26 -0700

    a fucntion that adds two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *add - adds two integers and returns the result.
+ *
+ *Return: the result
+ */
+
+int _abs(int a, int b)
+{
+	int result;
+	int sm;
+
+	sm = a + b;
+
+	return (result);
+}
