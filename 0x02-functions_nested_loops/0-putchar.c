860fe794238ec59421f3ba2955f115f549ef72ef
onzerebrian <onzerebrian@gmail.com>
2023-03-21 10:37:18 -0700

    printing _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - the begining of the program
+ *
+ * return(0) - returns 0 when the program is successful
+ *
+ */
+
+int main (void)
+{
+	printf("_putchar\n");
+	return (0);
+}
