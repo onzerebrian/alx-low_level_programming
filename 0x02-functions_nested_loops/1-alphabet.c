fa39d19fe1852431262097a3b77c907a5e3a825f
onzerebrian <onzerebrian@gmail.com>
2023-03-21 12:55:16 -0700

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - the beginning of the program
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	char a = 'a';
+
+	while (a<=z)
+	{
+	_putchar(&a);
+	a = a + 1;
+
+	}
+	_putchar('\n');
+	return (0);
+}
