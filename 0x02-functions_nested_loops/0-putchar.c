bb04a6161a26ae6c9281501bdd3609ef406e87bb
onzerebrian <onzerebrian@gmail.com>
2023-03-21 11:36:14 -0700

    created a ignore folder and content

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,7 +7,7 @@
  *
  */
 
-int main (void)
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
@@ -17,5 +17,6 @@ int main (void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	return(0);
+	_putchar('\n');
+	return (0);
 }
