489373a8e183a89a1691f0f4df354cdcab421009
onzerebrian <onzerebrian@gmail.com>
2023-03-21 14:25:37 -0700

    jack

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ *jack_bauer -timer
+ *
+ * @n: The character to be checked
+ *
+ *Return: 1 for lowercase character or 0 for anything else
+ */
+
+void jack_bauer(void)
+{
+	int a;
+	int b;
+	int c;
+	int d;
+
+	for (a = 0; a < 3; a++)
+	{
+	for (b = 0; b < 4; b++)
+	{
+	for (c = 0; c < 6; c++)
+	{
+	for (d = 0; d < 10; d++)
+	{
+		_putchar(a);
+		_putchar(b);
+		_putchar(':');
+		_putchar(c)
+		_putchar(d)
+		_putchar('\n');
+	}
+	}
+	}
+	}
+}
