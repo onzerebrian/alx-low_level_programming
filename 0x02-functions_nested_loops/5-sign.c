f198968fbcfdf967aa8441e61e546ad9fe0b7d30
onzerebrian <onzerebrian@gmail.com>
2023-03-21 13:58:27 -0700

    n

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - Checks for lowercase character
+ *
+ * @n: The character to be checked
+ *
+ *Return: 1 for lowercase character or 0 for anything else
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
