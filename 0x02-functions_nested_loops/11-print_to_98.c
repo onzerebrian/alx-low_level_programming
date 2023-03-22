48faf18e0fc36edfbcc50fd5fdbeafc83bd8b7c0
onzerebrian <onzerebrian@gmail.com>
2023-03-21 22:50:15 -0700

    all numbers upto 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include "stdio.h"
+
+/**
+ * print_to_98 - prints all numbers from the given number to 98
+ * @n: -s the input number
+ * Return: 0 when successful
+ */
+
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+			printf("98");
+	} else
+	{
+		while (n < 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
+			printf("98");
+	}
+	printf("\n");
+	return (0);
+}
