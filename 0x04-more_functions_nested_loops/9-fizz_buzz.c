01dba00e87600c12392166175be8ceb5633028e6
onzerebrian <onzerebrian@gmail.com>
2023-03-23 13:11:28 -0700

    Fizz-Buzz test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - the start of the program
+ *
+ * Return: 0 to mean success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+		printf("%d ", i);
+		}
+	}
+	putchar('\n');
+	return(0);
+}
