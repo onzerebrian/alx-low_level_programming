d38329e32c2a7cff1b90b599590124afa8eef907
onzerebrian <onzerebrian@gmail.com>
2023-03-23 22:01:21 +0300

    printing diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints a diagonal line
+ * @n: number of spaces
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+	int m;
+
+	for (i = 1; i <= n; i++)
+	{
+	for (m = 1; m <= i; m++)
+	{
+	_putchar(' ');
+	_putchar('\\');
+	}
+	}
+}
