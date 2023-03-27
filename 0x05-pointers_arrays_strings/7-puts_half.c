3ea1bce2d6d1e7303e2fddf045a56eaea36421e2
onzerebrian <onzerebrian@gmail.com>
2023-03-27 10:03:37 -0700

    [A

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts_half - prints second half of the characters
+ * @str: string pointer
+ */
+
+void puts_half(char *str)
+{
+	int x, i = 0, l = 0;
+
+	while (str[i] != '\0')
+	{
+		l++;
+		i++;
+	}
+	if (l % 2 == 0)
+	{
+		x = l / 2;
+	}
+	else
+	{
+		x = (l + 1) / 3;
+	}
+	for (i = x; i < l; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
