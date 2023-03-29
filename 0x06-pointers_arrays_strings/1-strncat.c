06d32d622664070f1be30c08ad33805dbd4a7a1e
onzerebrian <onzerebrian@gmail.com>
2023-03-29 11:00:45 -0700

    a function that concatenates two strings.

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncat - main function that copies src to the end of dest
+ * @dest: destination pointer
+ * @src: source pointer
+ * @n: bytes to be printed
+ * Return: returns the combine strings
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int len = 0;
+
+	while ((dest[i]) != '\0')
+	{
+		len = len + 1;
+		i++;
+	}
+
+	for (i = 0;i < n && (src[i]) != '\0'; i++)
+	{
+		dest[len + i] = src[i];
+	}
+	return (dest);
+}
