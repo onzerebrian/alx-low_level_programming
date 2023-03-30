f4e0d8b1a7fe5607aeaf43e930d394bf0d011772
onzerebrian <onzerebrian@gmail.com>
2023-03-30 08:56:09 -0700

    title case

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * cap_string - function for title case
+ * @str: string pointer
+ * Return: returns the convertred string
+ */
+
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+	if (str[i] >= 'a' && str[i] <= 'z')
+	{
+	if (str[i - 1] == ' ' ||
+	str[i - 1] == '\t' ||
+	str[i - 1] == '\n' ||
+	str[i - 1] == ',' ||
+	str[i - 1] == ';' ||
+	str[i - 1] == '.' ||
+	str[i - 1] == '!' ||
+	str[i - 1] == '?' ||
+	str[i - 1] == '"' ||
+	str[i - 1] == '(' ||
+	str[i - 1] == ')' ||
+	str[i - 1] == '{' ||
+	str[i - 1] == '}')
+	{
+		str[i] = str[i] - 32;
+	}
+	}
+	i++;
+	}
+	return (str);
+}
