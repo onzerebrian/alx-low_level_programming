f47b9f3a06c5d0ea0cad22a72bd4c10d0770e812
onzerebrian <onzerebrian@gmail.com>
2023-03-30 04:35:41 -0700

    int reverse

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * reverse_array - a function that reverses 
+ * @a: string pointer
+ * @n: number of elements of the array
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int x[1] = {0};
+	int t;
+
+	t = n;
+	if (n % 2 == 0)
+	{
+		n = n / 2;
+	}
+	else
+	{
+		n = (n + 1) / 2;
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		x[0] = a[i];
+		a[i] = a[t - 1 - i];
+		a[t - 1 - i] = x[0];
+	}
+}
