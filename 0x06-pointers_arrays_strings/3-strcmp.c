9026c98b389d4e90e2cc5b0718800e43dace8655
onzerebrian <onzerebrian@gmail.com>
2023-03-29 21:34:56 -0700

    string difference

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * strcmp - a function tha compares two strings
+ * @s1: a pointer to the first string
+ * @s2: a pointer to the second string
+ * Return: returns the diference
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int x;
+	int i;
+
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+	if (s1[i] == s2[i])
+	{
+		x = 0;
+	}
+	else
+	{
+		x = s1[i] - s2[i];
+			break;
+	}
+	}
+
+
+	return (x);
+}
