58e99218f30663fd7d99cc687958a0123eb9b9dd
onzerebrian <onzerebrian@gmail.com>
2023-04-08 04:41:21 -0700

    new

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,15 +2,15 @@
 #include <stdlib.h>
 
 /**
- * main - the strat of th eprogram
+ * main- the strat of th eprogram
  * @argc: number of arguments
  * @argv: the pointer to arguments
- * Retutn: returns 0 on success and 1 on failure
+ * Return: returns 0 on success and 1 on failure
  */
 int main(int argc, char *argv[])
 {
 	int num;
-	int coins;
+	int coins = 0;
 
 	if (argc != 2)
 	{
@@ -21,27 +21,27 @@ int main(int argc, char *argv[])
 	{
 		if (num >= 25)
 		{
-		coins = num / 25;
+		coins += num / 25;
 		num = num % 25;
 		}
 		else if (num >= 10 && num < 25)
 		{
-		coins = num / 10;
+		coins += num / 10;
 		num = num % 10;
 		}
 		else if (num >= 5 && num < 10)
 		{
-		coins = num / 5;
+		coins += num / 5;
 		num = num % 5;
 		}
 		else if (num >= 2 && num < 5)
 		{
-		coins = num / 2;
+		coins += num / 2;
 		num = num % 2;
 		}
 		else
 		{
-		coins = num / 1;
+		coins += num / 1;
 		num = num % 1;
 		}
 	}
