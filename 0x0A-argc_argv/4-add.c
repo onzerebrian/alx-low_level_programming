3737737d39bbb63768258cfb43d7670d59ce5948
onzerebrian <onzerebrian@gmail.com>
2023-04-08 02:57:05 -0700

    hot fix

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
 	if (argc ==  1)
 	{
 		printf("0\n");
-		return (0);
+		return (1);
 	}
 	else
 	{
@@ -36,6 +36,6 @@ int main(int argc, char *argv[])
 			}
 		}
 		printf("%d\n", m);
+		return (0);
 	}
-	return (0);
 }
