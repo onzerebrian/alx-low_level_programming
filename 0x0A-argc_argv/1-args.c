a136c41b2b77b59a7079cb3b3e97c3f5901a8048
onzerebrian <onzerebrian@gmail.com>
2023-04-08 01:12:06 -0700

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+
+
+/**
+
+ * main - start of the program
+
+ *
+
+ * @argc: the number of argunments
+
+ *
+
+ * @argv: the arguments enterred
+
+ *
+
+ * Return: returns 0 to mean success
+
+ */
+
+
+
+int main(int argc, char *argv[])
+{
+
+	int x;
+
+
+
+	x = argc - 1;
+
+	_putchar(x);
+	_putchar('\n');
+
+	return (0);
+}
+~
