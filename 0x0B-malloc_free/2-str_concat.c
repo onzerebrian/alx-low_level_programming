dc8ed0e4d3bc7ff0b1f8b27ebe8aa3b06bdc305d
onzerebrian <onzerebrian@gmail.com>
2023-04-11 14:12:54 -0700

    new

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,6 +13,10 @@ char *str_concat(char *s1, char *s2)
 	int len1 = 0, len2 = 0;
 	char *x;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 		len1++;
