deadc76b39415b8ac58768892d3e71ae2ba189ed
onzerebrian <onzerebrian@gmail.com>
2023-04-11 13:03:31 -0700

    n

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,6 +13,12 @@ char *_strdup(char *str)
 	char *x;
 	int len = 0;
 
+	if (str == NULL)
+	{
+	return (NULL);
+	}
+	else
+	{
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		len++;
@@ -20,12 +26,6 @@ char *_strdup(char *str)
 
 	x = malloc(len + 1);
 	
-	if (str == NULL)
-	{
-	return (NULL);
-	}
-	else
-	{
 	for (i = 0; i <= len; i++)
 	{
 		x[i] = str[i];
