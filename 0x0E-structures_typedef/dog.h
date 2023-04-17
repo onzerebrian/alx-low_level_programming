666953eaeec365c6cd0d14f92d94035dcf686ce0
onzerebrian <onzerebrian@gmail.com>
2023-04-17 14:34:08 -0700

    new

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,11 +6,17 @@
  * @name: name
  * @age: age
  * @owner: owner
+ * dog_t - ilias dog
  */
 
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
 struct dog
 {
 	char *name;
