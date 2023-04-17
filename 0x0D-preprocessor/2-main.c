8acf133cce595b526f3fe14384a5508739e844a0
onzerebrian <onzerebrian@gmail.com>
2023-04-17 12:27:21 -0700

    task 3

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,19 +1,5 @@
-#include <stdlib.h>
-#include <unistd.h>
+#include <stdio.h>
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-#define x __FILE__
 
 /**
  * main - the start of the program
@@ -21,11 +7,6 @@ int _putchar(char c)
  */
 int  main()
 {
-	int i;
-	char y[] = {x};
-
-	for (i = 0; y[i] != '\0'; i++)
-		_putchar(y[i]);
-	_putchar('\n');
+	printf("%s\n", __FILE__);
 	return (0);
 }
