9643362d93e738ade72fefdada0cc43104496052
onzerebrian <onzerebrian@gmail.com>
2023-04-27 22:17:23 +0300

    task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: list to be printed
+ * Return: number of nodes
+ */
+
+
+size_t print_list(const list_t *h)
+{
+	unsigned int i = 0;
+
+	while (h)
+	{
+	if (h->str == NULL)
+	{
+		printf("[0] (nil)\n");
+		i++;
+		h = h->next;
+	}
+	else
+	{
+		printf("[%d] %s\n", h->len, h->str);
+		i++;
+		h = h->next;
+	}
+	}
+	return (i);
+}
