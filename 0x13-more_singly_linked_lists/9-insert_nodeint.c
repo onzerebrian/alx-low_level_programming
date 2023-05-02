fede69f5757ca1c9b4a49053f8e9ca639dcfe90a
onzerebrian <onzerebrian@gmail.com>
2023-05-02 23:48:04 +0300

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,7 +19,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	if (*head == NULL)
 		return (NULL);
-	for (;(*head) != NULL; i++)
+	for (; (*head) != NULL; i++)
 	{
 		j++;
 		*head = (*head)->next;
