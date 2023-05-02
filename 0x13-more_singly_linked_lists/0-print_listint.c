e486fbea258bdb6a8dc3a58cb873068244bbf098
onzerebrian <onzerebrian@gmail.com>
2023-05-02 14:35:22 +0300

    task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,5 +8,17 @@
 
 size_t print_listint(const listint_t *h)
 {
+	unsigned int m;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+	for (m = 0; h != NULL; m++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (m);
 
 }
