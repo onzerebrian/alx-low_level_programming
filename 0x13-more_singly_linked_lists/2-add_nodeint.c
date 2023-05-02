a41dc36eb5295b5ab4bc2e84e894066b5b085c64
onzerebrian <onzerebrian@gmail.com>
2023-05-02 18:41:18 +0300

    task 2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -16,13 +16,13 @@ listint_t *add_nodeint(listint_t **head, const int n)
 		return (NULL);
 	if (*head == NULL)
 	{
+		new->n = n;
 		new->next = NULL;
 	}
 	else 
 	{
 	new->n = n;
 	new->next = *head;
-	
 	}
 	return (new);
 }
