6423d04b80a7ae46649312c278d9f61f2f429226
onzerebrian <onzerebrian@gmail.com>
2023-05-04 11:43:43 -0700

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,12 +14,14 @@ unsigned int binary_to_uint(const char *b)
 	if (b == NULL)
 		return (0);
 	for (i = 0; b[i] != '\0'; i++)
+	{
 		len++;
+	}
 	if (len == 1 && b[0] == '0')
 		return (0);
 	if (len == 1 && b[0] == '1')
 		return (1);
-	for (i = 0; i < len; i++)
+	for (i = 0; b[i] != '\0'; i++)
 	{
 	if (b[i] != '0' || b[i] != '1')
 		return (0);
