93c97258bc858403e3377a72d27b41627564cde1
onzerebrian <onzerebrian@gmail.com>
2023-05-04 11:29:35 -0700

    task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts binary to int
+ * @b: pointer to input char string
+ * Return: the converted int
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int r = 0, p = 1, j = 0;
+	unsigned int i, len = 0;
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		len++;
+	}
+	if (b == NULL)
+		return (0);
+
+	if (len == 1 && b[0] == '0')
+		return (0);
+	if (len == 1 && b[0] == '1')
+		return (1);
+	for (i = 0; i < len; i++)
+	{
+	if (b[i] != '0' || b[i] != '1')
+		return (0);
+	j = len - 1;
+	for(; j > 0; j--)
+	{
+		p *= 2
+	}
+	len --;
+	r += (p * (b[i] - 48));
+	p = 1;
+	}
+	return (r);
+
+}
