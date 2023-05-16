9f73ab8a239cd19fa6882b49463101d8a5ef0247
onzerebrian <onzerebrian@gmail.com>
2023-05-16 02:13:50 -0700

    task 0

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,9 +1,9 @@
-#include <stddef.h>
 #ifndef MAIN_H
 #define MAIN_H
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <stddef.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
