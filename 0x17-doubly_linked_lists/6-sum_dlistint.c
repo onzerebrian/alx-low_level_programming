29efc5ffb4bd5475e80a52ae87549a6f22b56e8a
onzerebrian <onzerebrian@gmail.com>
2023-06-15 06:14:43 -0700

    tak 6

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,3 +1,25 @@
 #include "lists.h"
 
+/**
+ * sum_dlistint - function
+ * @head: pointer
+ * Return: return value
+ */
+int sum_dlistint(dlistint_t *head)
+{
 
+	int sum;
+
+	sum = 0;
+
+	if (head == NULL)
+		return (0);
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+
+}
