1c4e71fe0f0f71e14ffed64e8dcb2f3818d6bcd8
onzerebrian <onzerebrian@gmail.com>
2023-06-15 05:59:50 -0700

    task 1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,3 +1,17 @@
 #include "lists.h"
 
+/**
+ * dlistint_len - function
+ * @h: pointer to first node
+ * Return: nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i;
 
+	if (h == NULL)
+		return (0);
+	for (i = 0; h != NULL; i++)
+		h = h->next;
+	return (i);
+}
