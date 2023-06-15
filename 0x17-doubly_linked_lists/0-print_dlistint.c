a63a2431b0b5aa34fd11730151fb0656654897a3
onzerebrian <onzerebrian@gmail.com>
2023-06-15 05:56:58 -0700

    task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,3 +1,22 @@
 #include "lists.h"
 
+/**
+ * print_dlistint - function
+ * @h: Pointer to the head node of a linked list
+ * Return: nodes on the list
+ */
 
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i;
+
+	if (h == NULL)
+		return (0);
+	for (i = 0; h != NULL; i++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+
+	}
+	return (i);
+}
