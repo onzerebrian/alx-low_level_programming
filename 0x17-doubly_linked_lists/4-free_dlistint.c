396ee3e26a5bcc72b551d2f57c1bdc4043f28b39
onzerebrian <onzerebrian@gmail.com>
2023-06-15 06:10:34 -0700

    task 4

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,3 +1,20 @@
 #include "lists.h"
 
+/**
+ * free_dlistint - function
+ * @head: pointer
+ * Return: return value
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *a;
 
+	while (head != NULL)
+	{
+		a = head;
+		head = head->next;
+		free(a);
+	}
+	free(head);
+
+}
