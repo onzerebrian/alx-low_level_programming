477d9af2ea423fef8b0b8ef54b70c8c6ac83d310
onzerebrian <onzerebrian@gmail.com>
2023-06-15 06:12:35 -0700

    task 5

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,3 +1,30 @@
 #include "lists.h"
 
+/**
+ * get_dnodeint_at_index - funtcion
+ * @head: pointer
+ * @index: index
+ * Return: return value
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
 
+	dlistint_t *x;
+	unsigned int cont;
+
+	cont = 0;
+	x = head;
+
+	if (head == NULL)
+		return (NULL);
+
+	while (index != cont)
+	{
+		if (x == NULL)
+			return (NULL);
+		x = x->next;
+		cont++;
+	}
+	return (x);
+
+}
